363f2b8bb58e577f045f0e093f9fbbd1455b33a0
Greg Meyer <abareasol@gmail.com>
2022-08-16 16:06:46 -0500

    Adding accelerator funcationality for VMware Explore (#51)
    * Adding logging pattern configuration option.
    
    Co-authored-by: gm2552 <meyerg@vmware.com>

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,6 +2,8 @@ package com.example.springboot;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +15,8 @@ import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 @SpringBootApplication
 public class Application {
 
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -21,12 +25,12 @@ public class Application {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			logger.info("Let's inspect the beans provided by Spring Boot:");
 
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
-				System.out.println(beanName);
+				logger.info(beanName);
 			}
 
 		};
