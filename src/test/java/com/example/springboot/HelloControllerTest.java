bebd1828a7a94cc7a89ea17c4b1e154f3d9f6804
shwethaks1 <94036575+shwethaks1@users.noreply.github.com>
2023-10-16 13:17:45 +0530

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu! commit -1"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu! commit -2"));
     }
 }
