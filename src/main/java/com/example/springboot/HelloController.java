98e44b71ad4d9b2c543e664338e20c760b65b0c2
shwethaks1 <94036575+shwethaks1@users.noreply.github.com>
2023-10-16 15:20:50 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!-commit-3";
+		return "Greetings from Spring Boot + Tanzu!-commit-latest";
 	}
 
 }
